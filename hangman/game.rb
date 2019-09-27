d23eb29bacbcd3580695f10e91ff9a3417eb62ba
Jomiro <jomiro.eming@gmail.com>
2019-09-27 19:07:33 +0200

    Fixed an error: The input==0 bit was spitting infinite puts... haha... now it waits for another input before moving

--- hangman/game.rb
+++ hangman/game.rb
@@ -1,57 +1,72 @@
+#
+# # start with 10 lives
+# lives = 10
+# words = ["interpolation","causal","ammunition","sanddune","blimp","ramen","wellington","eczema","artery","viable","asterisk"]
+# word = words.sample
+# guessed_word = "_" * word.length
+#
+# guessed_input =
+# correct_input
+#
+# require 'rainbow'
+#
+#   puts
+#   puts Rainbow("Welcome to HangMan! What's your name?").yellow
+#   puts
+#   name = gets.chomp
+#   puts
+#   puts Rainbow("Hey there, #{name}! :)").yellow
+#   puts Rainbow("Ready? Set? Here's your word...!").yellow
+#   puts
+#
+# while lives > 0 && guessed_word.include?("_")
+#
+#   puts guessed_word
+#
+#   input = $stdin.gets[0]
+#
+#   word.split("").each.with_index do |character, index|
+#       guessed_word[index] = character if character == input
+#     end
+#
+#
+#   if word.include?(input)
+#       puts Rainbow("Correct!").green.bold
+#
+#   else
+#     lives = lives-1
+#     life_word = lives == 1 ? "life" : "lives"
+#     puts Rainbow("Ru oh... you've got #{lives} #{life_word} left!").red.bold
+#   end
+#
+# end
+#
+#   if !guessed_word.include?("_")
+#     puts
+#     puts Rainbow("★______________★").green
+#     puts Rainbow("....YOU WIN!....").green.bold
+#     print "-> "
+#     puts Rainbow(guessed_word).orange
+#     puts Rainbow("★______________★").green
+#
+#
+#   else
+#     puts
+#     puts Rainbow("☠︎_______________☠︎").red
+#     puts Rainbow("....GAME OVER....").red.bold
+#     print "The word was "
+#     puts Rainbow(word).blue
+#     puts Rainbow("☠︎_______________☠︎").red
+#   end
+# puts
 
-# start with 10 lives
-lives = 10
-words = ["interpolation","causal","ammunition","sanddune","blimp","ramen","wellington","eczema","artery","viable","asterisk"]
-word = words.sample
-guessed_word = "_" * word.length
-
-require 'rainbow'
-
-  puts
-  puts Rainbow("Welcome to HangMan! What's your name?").yellow
-  puts
-  name = gets.chomp
-  puts
-  puts Rainbow("Hey there, #{name}! :)").yellow
-  puts Rainbow("Ready to play? Here's your word...").yellow
-  puts
-
-while lives > 0 && guessed_word.include?("_")
-
-  puts guessed_word
-
-  input = $stdin.gets[0]
-
-  word.split("").each.with_index do |character, index|
-      guessed_word[index] = character if character == input
-    end
-
-  if word.include?(input)
-      puts Rainbow("Correct!").green.bold
-
-  else
-    lives = lives-1
-    life_word = lives == 1 ? "life" : "lives"
-    puts Rainbow("Ru oh... you've got #{lives} #{life_word} left!").red.bold
-  end
-
+puts 'Type in as many words as you\'d like. When you\'re finished, press enter on an empty line'
+array = []
+input = ' '
+while input != ''
+  input = gets.chomp
+  array.push input
 end
 
-  if !guessed_word.include?("_")
-    puts
-    puts Rainbow("★______________★").green
-    puts Rainbow("....YOU WIN!....").green.bold
-    print "-> "
-    puts Rainbow(guessed_word).orange
-    puts Rainbow("★______________★").green
-
-
-  else
-    puts
-    puts Rainbow("☠︎_______________☠︎").red
-    puts Rainbow("....GAME OVER....").red.bold
-    print "The word was "
-    puts Rainbow(word).blue
-    puts Rainbow("☠︎_______________☠︎").red
-  end
 puts
+puts array.sort
--- tictactoe/game2.rb
+++ tictactoe/game2.rb
@@ -1,69 +1,3 @@
-# # let me input an X into the grid
-# # let me input an O into the grid
-# # repeat last two actions
-#
-# # when there's a row of 3 of either, tell me i win
-# # if all 9 spaces are filled, with no rows, tell me i lose
-#
-# require 'rainbow'
-#
-# row1 = ("123")
-# row2 = ("456")
-# row3 = ("789")
-#
-# grid_row1 = "_" * row1.length
-# grid_row2 = "_" * row2.length
-# grid_row3 = "_" * row3.length
-#
-# turn = 9
-#
-# tictac =  "x"
-# toe= "o"
-#
-# puts "LET THE GAMES BEGIN!"
-# puts
-#
-# while turn > 0
-#   puts
-#   puts grid_row1
-#   puts grid_row2
-#   puts grid_row3
-#   puts
-#
-#   input = $stdin.gets[0]
-#   turn = turn-1
-#
-#
-#     row1.split("").each.with_index do |character, index|
-#       if character == input
-#         grid_row1[index] = turn.odd? ? toe : tictac
-#       end
-#     end
-#     row2.split("").each.with_index do |character, index|
-#       if character == input
-#         grid_row2[index] = turn.odd? ? toe : tictac
-#       end
-#       end
-#     row3.split("").each.with_index do |character, index|
-#       if character == input
-#         grid_row3[index] = turn.odd? ? toe : tictac
-#       end
-#       end
-#
-# end
-#
-#     puts Rainbow("__________").red
-#     puts Rainbow("GAME OVER!").red
-#     puts Rainbow("__________").red
-#     puts grid_row1
-#     puts grid_row2
-#     puts grid_row3
-#
-#
-#
-# puts
-# puts
-
 require 'rainbow'
 
 rows = [
@@ -72,22 +6,25 @@ rows = [
   "789"
 ]
 
-# the board is always 3x3, so we can just hard-code it (rather than calculating it from rows)
 grid_rows = [
   "___",
   "___",
   "___"
 ]
 
-turn = 9
+board_full = false
+win = false
+row_number = nil
+column_number = nil
 
-tictac =  "x"
+tictac = "x"
 toe= "o"
+current_player = tictac
 
 puts
 puts Rainbow("LET THE GAMES BEGIN!").green.bold
 
-while turn > 0  # || grid_row.each.include?("_") (NEED TO ADD CONDITION OF THERE BEING A COMPLETE LINE!)
+while !board_full && !win
 
   puts
   grid_rows.each do |row|
@@ -96,34 +33,66 @@ while turn > 0  # || grid_row.each.include?("_") (NEED TO ADD CONDITION OF THERE
 
   puts
 
-  input = $stdin.gets[0].to_i
+  begin
 
-  # now that we have input from 1..9,
-  # calculate which row and column
-  row_number = (input-1) / 3
-  column_number = (input-1) % 3
+    input = $stdin.gets[0].to_i
 
-  player = turn.even? ? tictac : toe
+    while input == 0
+      puts
+      puts Rainbow("Ru oh... Try a number between 1 and 9!").orange.bold
+      puts
+      input = $stdin.gets[0].to_i
+      puts
+    end
 
-  grid_rows[row_number][column_number] = player
+    row_number = (input-1) / 3
+    column_number = (input-1) % 3
 
-  turn = turn-1
+  end while grid_rows[row_number][column_number] != "_"
+
+  grid_rows[row_number][column_number] = current_player
+
+  board_full = grid_rows.all? do |row|
+    row.split('').all? do |item|
+      item != "_"
+    end
+  end
+
+  win_row = grid_rows.any? do |item|
+    item == "xxx" || item == "ooo"
+  end
+
+  win_column = ["o","x"].any? do |player|
+    [0,1,2].any? do |column|
+      grid_rows.all? do |row|
+        row[column] == player
+      end
+    end
+  end
+
+  win_diagonal = ["o","x"].any? do |player|
+    (grid_rows[0][0] == player && grid_rows[1][1] == player && grid_rows[2][2] == player) ||
+    (grid_rows[0][2] == player && grid_rows[1][1] == player && grid_rows[2][0] == player)
+  end
+
+  win = win_row || win_column || win_diagonal
+
+  if current_player == tictac
+    current_player = toe
+  else
+    current_player = tictac
+  end
+
 end
 
-  # if any row is all x, say x wins
-  # if any row is all o, say o wins
-  # if any column is all x, say x wins
-  # if any column is all o, say o wins
-  # if x appears in r1c1 and r2c2 and r3c3, say x wins
-  # if o appears in r1c1 and r2c2 and r3c3, say o wins
-  # if x appears in r1c3 and r2c2 and r3c1, say x wins
-  # if o appears in r1c3 and r2c2 and r3c1, say o wins
-
-
+  if win
+    puts "HOLY SHIT"
+  else
 
   puts Rainbow("☠︎_______________☠︎").red
   puts Rainbow("....GAME OVER....").red.bold
   puts Rainbow("☠︎_______________☠︎").red
+end
 
   grid_rows.each do |row|
       puts row
