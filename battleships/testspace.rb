7207ff2bc9b476325bba3e36a392cc5b2946a6b6
Dan Davey <dan@offerzen.com>
2019-09-29 16:36:17 +0200

    adds to test space

--- battleships/testspace.rb
+++ battleships/testspace.rb
@@ -12,11 +12,22 @@ boards = [
 
 
 
+count = 0
+
   boards.each do |row|
+    
     bees = row.split("").count "B"
     sees = row.split("").count "C"
     esses = row.split("").count "S"
     hits = bees + sees + esses
+
+    count += hits
     puts hits.to_i
     puts
     end
+  end
+end
+
+puts count
+
+
