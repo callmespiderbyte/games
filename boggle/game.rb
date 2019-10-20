6784c421bacebe7593b86dd6b25ed12214248acb
Jomiro <jomiro.eming@gmail.com>
2019-10-20 17:42:34 +0200

    Words line up in grid + timer! Next: ANAGRAM!

--- boggle/game.rb
+++ boggle/game.rb
@@ -8,26 +8,57 @@
       # > if any word's letters are all present in the random 16 letters, display it
 
 require 'yaml'
+require 'rainbow'
+
+def linebreak(text)
+  puts
+  puts text
+  puts
+end
+
 
 alldicefile = YAML.load_file('alldice.yml')
 dice = alldicefile['alldice'].keys
 
 selection = []
 
+minutes = 1
+seconds = minutes * 60
+end_time = Time.now + seconds
+
 dice.each do |sides|
   letter = alldicefile['alldice'][sides].sample
   selection.push letter
 end
 
+# shuffle the order of the letters
+selection.shuffle!
+
+puts
+
 # take the selection of letters, and display them in four rows, each with four letters
+row1 = selection[0..3]
+row2 = selection[4..7]
+row3 = selection[8..11]
+row4 = selection[12..15]
+
+grid = [
+  row1,
+  row2,
+  row3,
+  row4,
+]
+
+grid.each do |row|
+  puts row.join(" ")
+end
+
+while Time.now < end_time
+end
+linebreak Rainbow("Time's up!").green.bold
+
 # (--> trying this one first)
 #
 # OR
 #
 # take the selection of letters, and replace each spot in the grid with one letter
-
-selection.each do |x|
- puts x.join(" ")
-end
-# ^ this returns: undefined method `join' for "T":String (NoMethodError)
-# also: 'code smell' > don't think i need "selection"... i can just include this in the other method, and use "letter" inside there?
