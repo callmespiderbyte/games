5cfdbaa9df764f4724e981b7bd8bb9ff6924e4e3
Jomiro <jomiro.eming@gmail.com>
2019-11-16 08:34:49 +0200

    adds things cant remember what am doing this a few days later oops

--- class_practice/classes.rb
+++ class_practice/classes.rb
@@ -12,19 +12,20 @@
 require 'rainbow'
 
 class Tamagotchi
-  attr_accessor :name, :age, :satiety, :energy, :affection, :happiness, :health
+  attr_accessor :name, :age, :satiety, :energy, :affection, :happiness, :health, :communism
 
-  def initialize(n, a, s, e, af, h)
+  def initialize(n, a, s, e, af, h, c)
      @name = n
      @age = a
      @satiety = s #.clamp(0, 10) <-- Not working here, so moved to loop action
      @energy = e #.clamp(0, 10)
      @affection = af #.clamp(0, 10)
      @happiness = h #.clamp(0, 10)
+     @communism = c #.clamp(0, 10)
      @health = s + e + af + h
    end
 
-   tamagotchi = Tamagotchi.new("", 0, 0, 0, 0, 0)
+   tamagotchi = Tamagotchi.new("", 0, 0, 0, 0, 0, 0)
 
     def feed
       @satiety += 2
@@ -32,6 +33,7 @@ class Tamagotchi
       @energy -= 1
       @happiness += 1
       @age += 0.2
+      @communism -= 1
       puts Rainbow("Yum! That tasted good!").red.bold
     end
 
@@ -39,6 +41,7 @@ class Tamagotchi
       @energy += 2
       @satiety -= 4
       @age += 0.5
+      @communism -= 2
       puts Rainbow("Zzz... It yawns and feels rested.").red.bold
     end
 
@@ -55,7 +58,17 @@ class Tamagotchi
       @energy -= 3
       @satiety -= 2
       @age += 0.2
+      @communism -= 1
       puts Rainbow("This is its favourite game!").red.bold
+    end
+
+    def russian_class
+      @happiness += 3
+      @energy -= -3
+      @satiety -= -1
+      @age += 0.2
+      @communism += 2
+      puts Rainbow("So school, much Russia, such Soviet, quite communism...!").red.bold
     end
 
 end
@@ -76,7 +89,7 @@ def orange_text(text)
   puts Rainbow(text).orange.bold
 end
 
-tamagotchi = Tamagotchi.new("Yaruko", 0, 7, 8, 10, 9)
+tamagotchi = Tamagotchi.new("Yaruko", 0, 7, 8, 10, 9, 10)
 
 puts
 orange_text "Meet your new pet-friend, #{tamagotchi.name}!"
@@ -106,12 +119,12 @@ while tamagotchi.age < 2
   input = $stdin.gets[0].to_i
   puts
 
-  while input < 1 || input > 4
-    rainbow_red "Oh dear... seems like you're trying to make #{tamagotchi.name} do things it doesn't know how to do!"
-    rainbow_red "Please stick to commands it knows?"
-    puts
-    input = $stdin.gets[0].to_i
-  end
+  # while input < 1 || input > 4
+  #   rainbow_red "Oh dear... seems like you're trying to make #{tamagotchi.name} do things it doesn't know how to do!"
+  #   rainbow_red "Please stick to commands it knows?"
+  #   puts
+  #   input = $stdin.gets[0].to_i
+  # end
 
   case input
     when 1
@@ -122,18 +135,64 @@ while tamagotchi.age < 2
       tamagotchi.love
     when 4
       tamagotchi.play
+    else
+      red_text "Roops! #{tamagotchi.name} doesn't know what that is yet..."
+      red_text "Please stick to commands it knows!"
   end
 
 end
 
 puts
-orange_text "WOAH, #{tamagotchi.name} grew up! Now it's a toddler! :D"
+orange_text "WOAH, #{tamagotchi.name} grew up! It's a toddler! :D"
+orange_text "It wants to learn about the Soviet Union!"
 puts
 
 while tamagotchi.age < 3
 
-  age_in_days = tamagotchi.age == 1 ? "day" : "days"
+  puts
+  puts "It's #{tamagotchi.age} days old, and its stats currently are:"
+  puts
+  green_text "Satiety:     #{tamagotchi.satiety.clamp(0,10)}/10"
+  green_text "Energy:      #{tamagotchi.energy.clamp(0,10)}/10"
+  green_text "Affection:   #{tamagotchi.affection.clamp(0,10)}/10"
+  green_text "Happiness:   #{tamagotchi.happiness.clamp(0,10)}/10"
+  green_text "Communism:   #{tamagotchi.communism.clamp(0,10)}/10"
+  # puts
+  # puts Rainbow("Health:      #{tamagotchi.health.clamp(0,40)}/40").green.bold
 
+  puts
+  yellow_text "What would you like to do?"
+  puts "1 = Feed #{tamagotchi.name}"
+  puts "2 = Put #{tamagotchi.name} to sleep"
+  puts "3 = Give #{tamagotchi.name} love"
+  puts "4 = Play with #{tamagotchi.name}"
+  puts "5 = Give #{tamagotchi.name} a lesson on Soviet Russia"
+  puts
+  input = $stdin.gets[0].to_i
+  puts
+
+  # while input < 1 || input > 4
+  #   rainbow_red "Oh dear... seems like you're trying to make #{tamagotchi.name} do things it doesn't know how to do!"
+  #   rainbow_red "Please stick to commands it knows?"
+  #   puts
+  #   input = $stdin.gets[0].to_i
+  # end
+
+  case input
+    when 1
+      tamagotchi.feed
+    when 2
+      tamagotchi.sleep
+    when 3
+      tamagotchi.love
+    when 4
+      tamagotchi.play
+    when 5
+      tamagotchi.russian_class
+    else
+      red_text "Roops! #{tamagotchi.name} doesn't know what that is yet..."
+      red_text "Please stick to commands it knows!"
+  end
 end
 
 
